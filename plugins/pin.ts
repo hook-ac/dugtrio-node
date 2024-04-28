151adffcada954a033a4fc4bb4cb752f42a1424c
cunev <cunevnew@gmail.com>
2024-04-28 15:35:13 -0700

    feat: improve pin plugin

--- plugins/pin.ts
+++ plugins/pin.ts
@@ -5,11 +5,9 @@ export function pin(): PluginResponse {
   return {
     setup: (self: Interactable) => {
       self.properties.offset = { x: 0, y: 0 };
-      if (!self.parent) {
-        throw "You need a parent interactable to pin";
-      }
     },
     draw: (self: Interactable) => {
+      if (!self.parent) return;
       self.position = {
         x: self.parent!.position.x + self.properties.offset.x,
         y: self.parent!.position.y + self.properties.offset.y,
